b6c7a74b66962e4a268cdcda5c8c45da3900ab1c
Jan Sladký <sladky.honza@gmail.com>
2025-07-24 12:46:44 +0200

    Feature: přidání loaderu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
-
+// src/app/layout.tsx
 import "../app/globals.css";
+import LoadingOverlay from "@/components/LoadingOverlay";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="cs">
-      <body className="font-sans bg-gray-100 text-gray-900">{children}</body>
+      <body className="font-sans bg-gray-100 text-gray-900">
+        <LoadingOverlay />  {/* Přidává spinner */}
+        {children}
+      </body>
     </html>
   );
 }
